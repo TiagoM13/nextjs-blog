cbebada0f91e191b24e840b07544377d092a8bcd
tiago-mota13 <tiago.mota@alive.app.br>
2022-09-21 00:28:28 -0300

    feat: add Head and Script

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,8 +1,20 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Script from 'next/script';
 
 export default function FirstPost() {
   return (
     <>
+    <Head>
+        <title>First Post</title>
+        <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnload"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+        />
+    </Head>
     <h1>First Post</h1>
     <h2>
       <Link href="/">Back to page</Link>
