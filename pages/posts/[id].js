185dce81e5044583a4c7bba3261f2313a27f39aa
tiago-mota13 <tiago.mota@alive.app.br>
2022-09-24 09:45:29 -0300

    feat: add styles to [id]

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -4,20 +4,21 @@ import Date from '../../components/date';
 
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
+import utilStyles from '../../styles/utils.module.css';
+
 export default function Post({ postData }) {
   return (
     <Layout>
       <Head>
         <title>{postData.title}</title>
       </Head>
-
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      <Date dateString={postData.date} />
-      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   );
 }
